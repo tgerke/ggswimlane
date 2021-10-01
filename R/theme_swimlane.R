a364f414614e7b122e3292209d2cb79487e4c351
Travis Gerke <tgerke@mail.harvard.edu>
2021-10-01 14:52:54 -0400

    correct bottom margin

--- R/theme_swimlane.R
+++ R/theme_swimlane.R
@@ -14,7 +14,7 @@
 #'   ggsci::scale_fill_jco() +
 #'   theme_swimlane(legend.position = c(.8, .1)) +
 #'   ggplot2::ggtitle("Time on study by subject") +
-#'   ggplot2::ylab("\nWeeks on study")
+#'   ggplot2::ylab("Weeks on study")
 theme_swimlane <- function(extra_margin_r = 15, ...) {
   ggplot2::theme_bw() +
   ggplot2::theme(
@@ -23,6 +23,9 @@ theme_swimlane <- function(extra_margin_r = 15, ...) {
     panel.grid.minor.y = ggplot2::element_blank(),
     panel.border = ggplot2::element_blank(),
     axis.ticks = ggplot2::element_blank(),
+    axis.title.x = ggplot2::element_text(
+      margin = ggplot2::margin(7.5, 0, 0, 0, "pt")
+    ),
     axis.title.y = ggplot2::element_blank(),
     legend.background = ggplot2::element_rect(
       fill="white", size = 0
